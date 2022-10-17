72a9ff83aac002d47b6c5fea3ff700645b7c6c63
Azamat <philthebird4393@gmail.com>
2022-10-17 18:15:19 +0500

    js-practice-1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
-"use strict";
+const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели', '');
+
+console.log(numberOfFilms);
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  private: false
+};
+
+const a = prompt('Один из просмотренных фильмов', ''),
+      b = prompt('На сколько оцените его', ''),
+      c = prompt('Один из просмотренных фильмов', ''),
+      d = prompt('На сколько оцените его', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+
+
 
 
